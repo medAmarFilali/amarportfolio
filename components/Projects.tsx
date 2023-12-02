eb115164c7e1f4cd22fd086a8933a05ab38f1a85
Med Amar FILALI <medamarfilali@gmail.com>
2023-12-02 18:02:37 +0100

    Added animations to the projects preview

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import adventure from "../public/images/adv.png";
 import abcdzImage from "../public/images/abcdzimage.png";
+import oneShop from "../public/images/oneshop.png";
 import ProjectElement from "./ProjectElement";
 import SectionHeading from "./SectionHeading";
 
@@ -8,6 +9,14 @@ export default function Projects() {
     <div id="projects">
       <SectionHeading title="Projects" />
       <div className="flex flex-col gap-4 group/list">
+        <ProjectElement
+          image={oneShop}
+          title="One Shop"
+          description="Web Store app that works as an interface for woocommerce and Dokan stores and substores, the app has a cart feature, follow store feature linked to the Dokan Pro feature and also a payment feature, I also made a dedicated plugin in wordpress to fetch the data and handle authentication."
+          link="#"
+          tags={["React Native", "Expo", "Wordpress", "Woocommerce"]}
+        />
+
         <ProjectElement
           image={adventure}
           title="WIGO OUTDOORS"
