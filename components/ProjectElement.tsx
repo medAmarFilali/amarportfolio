2da6b4296afe507b52d10b14d8e2c93ba228f35b
Med Amar FILALI <medamarfilali@gmail.com>
2023-12-02 17:34:58 +0100

    Made the a project element reusable component

--- components/ProjectElement.tsx
+++ components/ProjectElement.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
 import type { StaticImageData } from "next/image";
 import Chip from "./Chip";
+import { ArrowUpRight } from "lucide-react";
 
-export default function ProjectElement({ image }: { image: StaticImageData }) {
+export default function ProjectElement({
+  title,
+  link,
+  description,
+  image,
+  tags,
+}: {
+  title: string;
+  link: string;
+  description: string;
+  image: StaticImageData;
+  tags?: string[] | null;
+}) {
   return (
     <div className="mb-12 cursor-pointer">
       <div className="group relative flex flex-wrap gap-4 pb-1 transition-all sm:gap-8 lg:hover:!opacity-100 lg:group-hover/list:opacity-50 ">
@@ -11,21 +24,17 @@ export default function ProjectElement({ image }: { image: StaticImageData }) {
           <Image src={image} alt="wigoadventures" />
         </div>
         <div className="flex-1 z-10 ">
-          <h3 className="text-lg text-emerald-400 md:text-slate-200 font-semibold mb-2 group-hover:text-emerald-400 ">
-            WIGO OUTDOORS
-          </h3>
-          <p className="text-xs text-slate-400">
-            Mobile app that helps adventures looks for their next adventure, Geo
-            location, nearest adventures, adventures types (Camping, hiking,
-            biking...etc), Coachs Mobile app included with payment management
-            and google maps integration
-          </p>
+          <div className="flex gap-1 items-center mb-2">
+            <h3 className="text-lg text-emerald-400 md:text-slate-200 font-semibold group-hover:text-emerald-400 ">
+              {title}
+            </h3>
+            <div className="w-4 h-4 overflow-hidden">
+              <ArrowUpRight className="w-4 h-4 group-hover:text-emerald-400 transition-transform translate-y-4 -translate-x-4 group-hover:translate-y-0 group-hover:translate-x-0    " />
+            </div>
+          </div>
+          <p className="text-xs text-slate-400">{description}</p>
           <div className="flex flex-row gap-2 mt-4 flex-wrap ">
-            <Chip text="React native" />
-            <Chip text="Redux" />
-            <Chip text="expo" />
-            <Chip text="Supabase" />
-            <Chip text="PostGIS" />
+            {tags && tags.map((tag) => <Chip text={tag} key={tag} />)}
           </div>
         </div>
       </div>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import adventure from "../public/images/adv.png";
+import abcdzImage from "../public/images/abcdzimage.png";
 import ProjectElement from "./ProjectElement";
 import SectionHeading from "./SectionHeading";
 
@@ -8,12 +8,30 @@ export default function Projects() {
     <div id="projects">
       <SectionHeading title="Projects" />
       <div className="flex flex-col gap-4 group/list">
-        <ProjectElement image={adventure} />
-        <ProjectElement image={adventure} />
-        <ProjectElement image={adventure} />
-        <ProjectElement image={adventure} />
-        <ProjectElement image={adventure} />
-        <ProjectElement image={adventure} />
+        <ProjectElement
+          image={adventure}
+          title="WIGO OUTDOORS"
+          description="Mobile app that helps adventures looks for their next adventure, Geo
+            location, nearest adventures, adventures types (Camping, hiking,
+            biking...etc), Coachs Mobile app included with payment management
+            and google maps integration"
+          link="#"
+          tags={["React Native", "Redux", "Expo", "Supabase", "PostGIS"]}
+        />
+        <ProjectElement
+          image={abcdzImage}
+          title="e-ABCDZ"
+          description="Educational mobile app for french lessons, The app includes course subscriptions, Live courses, video courses and quizzes with an React admin web app to manage users, payments and subscriptions."
+          link="#"
+          tags={[
+            "React Native",
+            "React",
+            "Expo",
+            "NodeJS",
+            "Stripe",
+            "Google API",
+          ]}
+        />
       </div>
     </div>
   );
