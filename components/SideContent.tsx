35b1e5c74f1a86d3dcee00fd4ab083e3260f24dd
Med Amar FILALI <medamarfilali@gmail.com>
2023-12-03 23:27:08 +0100

    Fixed async component issue

--- components/SideContent.tsx
+++ components/SideContent.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Bio from "./Bio";
 import ExpElement from "./ExpElement";
 import Experience from "./Experience";
@@ -8,6 +9,7 @@ export default function SideContent() {
   return (
     <div className="lg:sticky lg:top-0 lg:flex lg:w-1/2 lg:flex-col lg:justify-between lg:pt-24 pt-16 ">
       <Bio />
+      {/* @ts-expect-error Async Server Component */}
       <LatestPosts />
       <Projects />
       <Experience />
